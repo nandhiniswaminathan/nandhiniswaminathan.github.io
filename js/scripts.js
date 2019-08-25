435ad61671ba750df1fe9bdca0f9ee0713bd7854
Samuel Showalter <sshowalter@westmonroepartners.com>
2019-08-24 19:14:45 -0500

    Finally fast scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -15,40 +15,26 @@
     // Remove no-js class
     $('html').removeClass('no-js');
 
-    // Animate to section when nav is clicked
-    $('header a').click(function(e) {
+    // // Animate to section when nav is clicked
+    // $('header a').click(function(e) {
 
-        // Treat as normal link if no-scroll class
-        if ($(this).hasClass('no-scroll')) return;
+    //     // Treat as normal link if no-scroll class
+    //     if ($(this).hasClass('no-scroll')) return;
 
-        e.preventDefault();
-        var heading = $(this).attr('href');
-        var scrollDistance = $(heading).offset().top;
+    //     e.preventDefault();
+    //     var heading = $(this).attr('href');
+    //     var scrollDistance = $(heading).offset().top;
 
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+    //     $('html, body').animate({
+    //         scrollTop: scrollDistance + 'px'
+    //     }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
 
-        // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
-            $('header, body').removeClass('active');
-        }
-    });
+    //     // Hide the menu once clicked if mobile
+    //     if ($('header').hasClass('active')) {
+    //         $('header, body').removeClass('active');
+    //     }
+    // });
 
-    // Scroll to top
-    $('#to-top').click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 400);
-    });
-
-    // Scroll to first element
-    $('#lead-down span').click(function() {
-        var scrollDistance = $('#lead').next().offset().top;
-        $('html, body').animate({
-            scrollTop: scrollDistance + 'px'
-        }, 400);
-    });
 
     // Create timeline
     $('#experience-timeline').each(function() {
