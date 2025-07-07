b896e7861a169f7a7389989ce732564240322b5b
Nandhini <nandhini.s.nitt@gmail.com>
2025-07-06 21:02:16 -0700

    Personalized website design and structure

--- js/vanta.birds.js
+++ js/vanta.birds.js
@@ -576,9 +576,9 @@ class Birds extends VantaBase {
   static initClass() {
     this.prototype.defaultOptions = {
       // Beige: 0xf8e8d0, 0xf50000, 0xcfcf1d
-      backgroundColor: 0x#3498db, // 0x202428
-      color1: 0xff0000, // 0xf50000 # 0xfa9898
-      color2: 0x00d1ff, // 0xcfcf1d # 0x8c4646
+      backgroundColor: 0x202428, // 0x202428
+      color1: 0xffffff,// 0xf50000 # 0xfa9898
+      color2: 0xaaaaaa,// 0xcfcf1d # 0x8c4646
       colorMode: 'lerpGradient',
       birdSize: 1,
       wingSpan: 30,
